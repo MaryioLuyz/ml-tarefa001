803bc3c755e437b4fedb9c83c3c9b954adcf6000
maryo <maryo@MARIO-LUIZ.com>
2022-07-09 06:44:24 -0300

    Removed history, due to sensitive data

---
+++ backend/src/main/java/com/ml/tarefa001/Tarefa001Application.java
@@ -0,0 +1,13 @@
+package com.ml.tarefa001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarefa001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarefa001Application.class, args);
+	}
+
+}
