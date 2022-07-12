4cc1614be6572e45b57a0b6c0aa603cc8b069d66
maryo <maryo@MARIO-LUIZ.com>
2022-07-12 00:02:10 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category(name) VALUES ('Lucio');
-INSERT INTO tb_category(name) VALUES ('Carlos');
-INSERT INTO tb_category(name) VALUES ('Felipe');
+INSERT INTO tb_category(name, created_At) VALUES ('Lucio', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Carlos', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Felipe', NOW());
