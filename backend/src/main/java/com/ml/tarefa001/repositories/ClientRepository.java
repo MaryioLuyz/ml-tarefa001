6cca4391586aba7306a1207337320436970491e4
maryo <maryo@MARIO-LUIZ.com>
2022-07-19 15:34:06 -0300

    Client repository, service, resource, GET ok

---
+++ backend/src/main/java/com/ml/tarefa001/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.ml.tarefa001.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ml.tarefa001.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
